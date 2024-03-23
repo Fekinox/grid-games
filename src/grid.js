529302a79c0c06e15d600d96c27be17fa097b485
Fekinox <williamowenfowlk@gmail.com>
2024-03-23 18:52:00 -0400

    Begin linting process

--- src/grid.js
+++ src/grid.js
@@ -9,115 +9,115 @@ class Grid {
     { x: -1, y: 0, }, // W
     { x: -1, y: 1, }, // SW
     { x: 0, y: 1, }, // S
-  ]
+  ];
 
   static forwardDirections = [
     { x: 1, y: 0, }, // E
     { x: 0, y: 1, }, // S
     { x: 1, y: -1, }, // NE
     { x: 1, y: 1, }, // SE
-  ]
+  ];
 
   // Constructs a new grid given a width and a height.
   // If mapFun is provided, then the (x, y) cell of the grid will be
   // mapFun(x, y).
   constructor(width, height, mapFun = null) {
-    this.width = width
-    this.height = height
-    let f = () => null
+    this.width = width;
+    this.height = height;
+    let f = () => null;
     if (mapFun !== null) { 
       f = (v, idx) => {
-        return mapFun(idx % this.width, Math.floor(idx / this.width))
-      }
+        return mapFun(idx % this.width, Math.floor(idx / this.width));
+      };
     } 
     this.grid = 
       Array.from({ length: this.width * this.height },
-      f)
+        f);
   }
 
   // Gets the index within the grid array.
   index(x, y) {
-    return x + (y * this.width)
+    return x + (y * this.width);
   }
 
   // Returns true if the given coordinates are within the grid bounds.
   inBounds(x, y) {
-    return 0 <= x && x < this.width && 0 <= y && y < this.height
+    return 0 <= x && x < this.width && 0 <= y && y < this.height;
   }
 
   // Retrieve a reference to element (x, y) in the grid. Returns null
   // if the index is out of bounds.
   get(x, y) {
-    if (!this.inBounds(x, y)) { return null }
-    return this.grid[this.index(x, y)]
+    if (!this.inBounds(x, y)) { return null; }
+    return this.grid[this.index(x, y)];
   }
 
   // Set the element at (x, y) to the given value. Will do nothing if
   // the given coordinates are out of bounds.
   set(x, y, v) {
-    if (!this.inBounds(x, y)) { return null }
-    this.grid[this.index(x, y)] = v
+    if (!this.inBounds(x, y)) { return null; }
+    this.grid[this.index(x, y)] = v;
   }
 
   // Produces a shallow copy of the current grid.
   clone() {
-    let clone = new Grid(this.width, this.height)
-    clone.grid = [...this.grid]
-    return clone
+    let clone = new Grid(this.width, this.height);
+    clone.grid = [...this.grid];
+    return clone;
   }
 
   // Scans the grid in the given direction (dx, dy) and returns the results as
   // an array of objects (x, y, elem).
   lineQuery(x, y, dx, dy) {
-    let res = []
+    let res = [];
 
-    let xx = x
-    let yy = y
+    let xx = x;
+    let yy = y;
 
     while (this.inBounds(xx, yy)) {
       res.push({
         x: xx,
         y: yy,
         elem: this.get(xx, yy)
-      })
-      xx += dx
-      yy += dy
+      });
+      xx += dx;
+      yy += dy;
     }
 
-    return res
+    return res;
   }
 
   // Searches the grid for k consecutive elements equal to `elem` in a row.
   kInARow(x, y, dx, dy, length, elem) {
-    const endX = x + dx*(length - 1)
-    const endY = y + dy*(length - 1)
+    const endX = x + dx*(length - 1);
+    const endY = y + dy*(length - 1);
     if (!this.inBounds(endX, endY) ||
       !this.inBounds(x, y) ||
-      this.get(x, y) !== elem) { return null }
+      this.get(x, y) !== elem) { return null; }
 
-    let res = []
+    let res = [];
 
-    let xx = x
-    let yy = y
-    let steps = 0
+    let xx = x;
+    let yy = y;
+    let steps = 0;
 
     while (this.inBounds(xx, yy) && steps < length) {
-      if (this.get(xx, yy) !== elem) { return null }
+      if (this.get(xx, yy) !== elem) { return null; }
       res.push({
         x: xx,
         y: yy,
-      })
-      xx += dx
-      yy += dy
-      steps += 1
+      });
+      xx += dx;
+      yy += dy;
+      steps += 1;
     }
 
-    return res
+    return res;
   }
 
   // Finds all k-in-a-rows of length `length` with elem `elem`
   allKInARows(length, elem) {
-    let results = []
+    let results = [];
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
         [
@@ -126,38 +126,38 @@ class Grid {
           this.kInARow(x, y, 1, 1, length, elem),
           this.kInARow(x, y, 1, -1, length, elem),
         ].forEach((res) => {
-          if (res !== null) { results.push(res) }
-        })
+          if (res !== null) { results.push(res); }
+        });
       }
     }
 
-    return results
+    return results;
   }
 
   // Replaces the grid with a new grid with dimensions newWidth and newHeight
   // and the entries of the old grid being placed at (originX, originY), with
   // all other values being null.
   resize(newWidth, newHeight, originX, originY) {
-    let g = this
+    let g = this;
     let newGrid =
       Array.from(
         { length: newWidth * newHeight },
         (v, idx) => {
-          const curX = idx % newWidth
-          const curY = Math.floor(idx / newWidth)
+          const curX = idx % newWidth;
+          const curY = Math.floor(idx / newWidth);
 
-          const offsetX = curX - originX
-          const offsetY = curY - originY
+          const offsetX = curX - originX;
+          const offsetY = curY - originY;
 
           if (g.inBounds(offsetX, offsetY)) {
-            return g.get(offsetX, offsetY)
+            return g.get(offsetX, offsetY);
           } else {
-            return null
+            return null;
           }
         }
-      )
-    this.width = newWidth
-    this.height = newHeight
-    this.grid = newGrid
+      );
+    this.width = newWidth;
+    this.height = newHeight;
+    this.grid = newGrid;
   }
 }
--- src/gridview.js
+++ src/gridview.js
@@ -7,122 +7,122 @@
 class GridView {
   constructor(parentElement) {
     // The last focused grid element.
-    this.lastFocus = null
+    this.lastFocus = null;
 
-    this.onclick = (pos) => {
-    }
+    this.onclick = (_pos) => {
+    };
 
-    this.onhover = (pos) => {
-    }
+    this.onhover = (_pos) => {
+    };
 
     // The container for all the grid's rows and cells.
-    this.gridItem = elementBuild('div', {
-      id: 'tttgrid',
+    this.gridItem = elementBuild("div", {
+      id: "tttgrid",
       parent: parentElement,
-    })
+    });
 
-    this.gridItem.addEventListener('click', (evt) => {
-      if (!evt.target.classList.contains('tttcell')) { return }
+    this.gridItem.addEventListener("click", (evt) => {
+      if (!evt.target.classList.contains("tttcell")) { return; }
 
       this.onclick({ 
         x: Number(evt.target.dataset.x),
         y: Number(evt.target.dataset.y),
-      })
-    })
+      });
+    });
 
-    this.gridItem.addEventListener('mouseover', (evt) => {
-      let newFocus = null
+    this.gridItem.addEventListener("mouseover", (evt) => {
+      let newFocus = null;
 
-      if (evt.target.classList.contains('tttcell')) {
+      if (evt.target.classList.contains("tttcell")) {
         newFocus = {
           x: Number(evt.target.dataset.x),
           y: Number(evt.target.dataset.y),
-        }
+        };
       }
 
       const oneIsNull =
         this.lastFocus === null && newFocus !== null ||
-        this.lastFocus !== null && newFocus === null
+        this.lastFocus !== null && newFocus === null;
       const bothNonNull =
-        this.lastFocus !== null && newFocus !== null
+        this.lastFocus !== null && newFocus !== null;
       const differentValues =
         bothNonNull && (this.lastFocus.x !== newFocus.x ||
-          this.lastFocus.y !== newFocus.y)
+          this.lastFocus.y !== newFocus.y);
       if (oneIsNull || differentValues) {
-        this.lastFocus = newFocus
-        this.onhover(newFocus)
+        this.lastFocus = newFocus;
+        this.onhover(newFocus);
       }
-    })
+    });
   }
 
   // Rebuilds the grid in the DOM. The provided buildFn will be passed
   // the x, y, and current cell (as an object containing the cell and hbox)
   // to initialize the object.
   buildNewGrid(width, height, buildFn = null) {
-    let newFun = (x, y, cell) => {}
+    let newFun = (_x, _y, _cell) => {};
 
     if (buildFn !== null) {
-      newFun = buildFn
+      newFun = buildFn;
     }
 
-    this.gridItem.textContent = ''
+    this.gridItem.textContent = "";
     this.renderableGrid = 
       new Grid(width, height,
-      (x, y) => {
-        let cell = elementBuild('div', {
-          classList: 'tttcell',
-        })
+        (x, y) => {
+          let cell = elementBuild("div", {
+            classList: "tttcell",
+          });
 
-        let cellHoverBox = elementBuild('div', {
-          classList: 'cellhoverbox',
-          parent: cell,
-        })
+          let cellHoverBox = elementBuild("div", {
+            classList: "cellhoverbox",
+            parent: cell,
+          });
 
-        cell.dataset.x = x
-        cell.dataset.y = y
+          cell.dataset.x = x;
+          cell.dataset.y = y;
 
-        let obj = {
-          cell: cell,
-          hbox: cellHoverBox,
-        }
+          let obj = {
+            cell: cell,
+            hbox: cellHoverBox,
+          };
 
-        newFun(x, y, obj)
+          newFun(x, y, obj);
 
-        return obj
-      })
+          return obj;
+        });
 
     for (let y = 0; y < height; y++) {
-      let row = elementBuild('div', {
-        classList: 'tttrow',
+      let row = elementBuild("div", {
+        classList: "tttrow",
         parent: this.gridItem,
-      })
+      });
       for (let x = 0; x < width; x++) {
         let cell = this.renderableGrid.get(x, y);
-        row.appendChild(cell.cell)
+        row.appendChild(cell.cell);
       }
     }
   }
 
   update(x, y, newClasses) {
-    let cell = this.renderableGrid.get(x, y)
-    cell.cell.className = 'tttcell'
-    let trimmed = newClasses.trim()
+    let cell = this.renderableGrid.get(x, y);
+    cell.cell.className = "tttcell";
+    let trimmed = newClasses.trim();
     if (trimmed.length !== 0) {
-      for (const c of trimmed.split(' ')) {
-        cell.cell.classList.add(c)
+      for (const c of trimmed.split(" ")) {
+        cell.cell.classList.add(c);
       }
     }
   }
 
   getCell(x, y) {
-    return this.renderableGrid.get(x, y).cell
+    return this.renderableGrid.get(x, y).cell;
   }
 
   getHbox(x, y) {
-    return this.renderableGrid.get(x, y).hbox
+    return this.renderableGrid.get(x, y).hbox;
   }
 
   animate(x, y, animationName, params = {}) {
-    applyAnimation(this.renderableGrid.get(x, y).cell, animationName, params)
+    applyAnimation(this.renderableGrid.get(x, y).cell, animationName, params);
   }
 }
